fe48bbd786a1a9bf0d91dd7b1236ee96c2c22eb4
MThuro <mareike.thurau@t-online.de>
2015-10-28 09:09:59 +0100

    Eingabefelder AddFridgeItem

--- app/src/main/java/bjoernbinzer/myvirtualfridge/AddFridgeItem.java
+++ app/src/main/java/bjoernbinzer/myvirtualfridge/AddFridgeItem.java
@@ -7,6 +7,8 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class AddFridgeItem extends AppCompatActivity {
 
@@ -27,5 +29,13 @@ public class AddFridgeItem extends AppCompatActivity {
                 finish();
             }
         });
+        Spinner spinner = (Spinner) findViewById(R.id.UOM);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.UOM_entries, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
     }
 }
