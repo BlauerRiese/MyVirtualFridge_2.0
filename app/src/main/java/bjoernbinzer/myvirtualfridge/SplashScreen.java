02de0297010eaf09702dc4d12974e63a6334a915
zarfty <benz.tamara@gmail.com>
2015-11-03 01:41:57 +0100

    Splash Screen with Logo on App Start

---
+++ app/src/main/java/bjoernbinzer/myvirtualfridge/SplashScreen.java
@@ -0,0 +1,33 @@
+package bjoernbinzer.myvirtualfridge;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class SplashScreen extends AppCompatActivity {
+    private static int SPLASH_TIME_OUT = 2000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreen.this, StartActivity.class);
+                startActivity(intent);
+
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+
+    }
+
+}
