636ccde3130002b2d63bb29061e0c1b80e59c87d
BlauerRiese <bjoern@b-binzer.de>
2015-11-04 18:00:51 +0100

    Better names

---
+++ app/src/main/java/bjoernbinzer/myvirtualfridge/RoundCornerLinearLayout.java
@@ -0,0 +1,26 @@
+package bjoernbinzer.myvirtualfridge;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.LinearLayout;
+
+/**
+ * Created by BJOERN on 04.11.2015.
+ */
+public class RoundCornerLinearLayout extends LinearLayout {
+    public RoundCornerLinearLayout(Context context) {
+        super(context);
+    }
+
+    public RoundCornerLinearLayout(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public RoundCornerLinearLayout(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    public RoundCornerLinearLayout(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+}
